3f690c5f5eb39d6c696be59f752344e2760397e3
yash <yashajaymagar10@gmail.com>
2025-10-20 09:26:10 +0530

    Added next things

--- ems-web/backend/db.js
+++ ems-web/backend/db.js
@@ -3,13 +3,13 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || 'localhost',
-  user: process.env.DB_USER || 'ems_web',
-  password: process.env.DB_PASS || 'Demo@123',
-  database: process.env.DB_NAME || 'ems_cloud',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
+  ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: false } : null,
 });
 
 module.exports = pool;
